bf94246f12b881573bce4b71c3948078bcfbc2ae
Matt Taylor <taylor.matt777@gmail.com>
2021-03-14 01:15:43 -0700

    Show tabs for registration and bracket when admin

--- src/app/components/bracket-event/bracket-event.component.ts
+++ src/app/components/bracket-event/bracket-event.component.ts
@@ -1,4 +1,6 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {ADMINS} from 'constants';
+import {GeneralsService} from 'src/app/services/generals.service';
 import {EventStatus, IDoubleElimEvent, IDoubleEliminationBracket, ILeaderboardPlayer, IMatchTeam, MatchTeamStatus} from 'types';
 import {getShuffledBracket} from './bracket-creator';
 
@@ -18,15 +20,41 @@ export class BracketEventComponent {
 
   bracket: IDoubleEliminationBracket;
 
-  constructor() {}
+  constructor(
+      private readonly generals: GeneralsService,
+  ) {}
 
-  get showRegistration(): boolean {
+  selectedTab = 'Registration';
+
+  get registrationOpen(): boolean {
     // TODO, show the registration component up until the bracket has been
     // completed, the start time isn't a hard start by any means
     // we should show some sort of messaging to players though that they are
     // waiting for the event organizers to start the tournament
-    const bracketCreated = false;
-    return this.status === EventStatus.UPCOMING || !bracketCreated;
+    return this.status === EventStatus.UPCOMING;
+  }
+
+  get showRegistration(): boolean {
+    return this.selectedTab === 'Registration' && this.registrationOpen;
+  }
+
+  get tabs(): string[] {
+    const tabs = [];
+
+    if (this.registrationOpen) {
+      tabs.push('Registration');
+    }
+
+    if (ADMINS.includes(this.generals.name)) {
+      tabs.push('Bracket');
+    }
+
+    return tabs;
+  }
+
+  selectTab(tab: string) {
+    console.log(tab);
+    this.selectedTab = tab;
   }
 
   // TODO: this will be a real flow for just admins, and it will use the checked
