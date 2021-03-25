a5d7ded258edc23f320fb683ba84094f7b2e17b6
Matt Taylor <taylor.matt777@gmail.com>
2021-03-25 17:52:51 -0600

    Rules update to properly show "best of X"

--- src/app/components/rules/rules.component.ts
+++ src/app/components/rules/rules.component.ts
@@ -31,6 +31,10 @@ export class RulesComponent {
     return this.event?.format === EventFormat.DOUBLE_ELIM;
   }
 
+  bestOf(winningSets: number): string {
+    return `best ${winningSets} of ${winningSets * 2 - 1}`;
+  }
+
   toggleShowRules() {
     this._showRules = !this._showRules;
   }
