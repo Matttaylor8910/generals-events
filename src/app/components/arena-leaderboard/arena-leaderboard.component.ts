b39d675d1fe0b3eac5295c0c7f125d6e6db71179
Matt Taylor <taylor.matt777@gmail.com>
2021-03-28 23:45:36 -0600

    Finalize arena results

--- src/app/components/arena-leaderboard/arena-leaderboard.component.ts
+++ src/app/components/arena-leaderboard/arena-leaderboard.component.ts
@@ -1,6 +1,7 @@
 import {Component, EventEmitter, Input, Output, SimpleChanges} from '@angular/core';
 import {EventService} from 'src/app/services/event.service';
 import {GeneralsService} from 'src/app/services/generals.service';
+import {UtilService} from 'src/app/services/util.service';
 
 import {ADMINS} from '../../../../constants';
 import {EventStatus, EventType, IArenaEvent, ILeaderboardPlayer} from '../../../../types';
@@ -29,11 +30,10 @@ export class ArenaLeaderboardComponent {
   inEvent = false;
   recentlyJoined = false;
 
-  absentPlayers: ILeaderboardPlayer[] = [];
-
   constructor(
       public readonly generals: GeneralsService,
       private readonly eventService: EventService,
+      private readonly utilService: UtilService,
   ) {
     this.toggleTracking();
   }
@@ -41,7 +41,6 @@ export class ArenaLeaderboardComponent {
   ngOnChanges(changes: SimpleChanges) {
     if (changes.players) {
       this.determineInEvent();
-      this.determineAbsentPlayers();
       this.setVisible();
     }
   }
@@ -74,9 +73,9 @@ export class ArenaLeaderboardComponent {
     ].includes(this.status);
   }
 
-  get showPrune(): boolean {
+  get showFinalizeButton(): boolean {
     return ADMINS.includes(this.generals.name) &&
-        this.status === EventStatus.FINISHED && this.absentPlayers.length > 0;
+        this.status === EventStatus.FINISHED && !this.event?.winners?.length;
   }
 
   get pageControlText(): string {
@@ -171,15 +170,34 @@ export class ArenaLeaderboardComponent {
         this.players && !!this.players.find(p => p.name === this.generals.name);
   }
 
-  determineAbsentPlayers() {
-    this.absentPlayers =
-        this.players.filter(player => !player.stats?.totalGames);
-  }
+  async finalizeTournament() {
+    // remove all players from the event that played 0 games
+    const absentPlayersPromises =
+        this.players.filter(player => !player.stats?.totalGames).map(player => {
+          return this.eventService.removePlayer(this.event.id, player.name);
+        });
 
-  async prunePlayers() {
-    for (const player of this.absentPlayers) {
-      console.log(`${player.name} didn't play, pruning...`);
-      this.eventService.removePlayer(this.event.id, player.name);
+    // remove all players simulutaneously
+    if (absentPlayersPromises.length > 0) {
+      await Promise.all(absentPlayersPromises);
+    }
+
+    // prompt to delete the tournament if no one played any games
+    if (!this.event.completedGameCount) {
+      const shouldDelete = await this.utilService.confirm(
+          'Delete event?',
+          'No one showed up and played any games, delete this event?', 'Delete',
+          'Cancel');
+      if (shouldDelete) {
+        this.eventService.deleteEvent(this.event.id);
+      }
+    }
+
+    // then set the winner of this arena
+    else {
+      this.eventService.updateEvent(this.event.id, {
+        winners: [this.players[0].name],
+      });
     }
   }
 }
