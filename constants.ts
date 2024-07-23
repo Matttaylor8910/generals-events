94ab09cbe30126114aaaee436e859a571e7276b7
Matt Taylor <taylor.matt777@gmail.com>
2024-07-22 18:38:15 -0700

    Add new mods

--- constants.ts
+++ constants.ts
@@ -20,7 +20,9 @@ export const ADMINS = [
   'pasghetti',
   'Wuped',
   'Fekete',
-  'mashiro'
+  'mashiro',
+  'EklipZ',
+  'ZekeBeastie',
 ];
 
 export const HIDE_COMPLETED = 'generals-hide-completed';
