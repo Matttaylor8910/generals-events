f2ada7cb153dceaf473b3ee02722841f1c3ae590
Travis Drake <tdrake0x45@gmail.com>
2024-07-25 08:43:59 -0700

    New mods and options support, eklipz server support, local server support

--- constants.ts
+++ constants.ts
@@ -2,12 +2,16 @@ export enum GeneralsServer {
   NA = 'na',
   EU = 'eu',
   BOT = 'bot',
+  EKLIPZ = 'ek',
+  LOCAL = 'loc'
 }
 
 export const SITE_URLS = {
   [GeneralsServer.NA]: 'https://generals.io',
   [GeneralsServer.EU]: 'https://eu.generals.io',
   [GeneralsServer.BOT]: 'https://bot.generals.io',
+  [GeneralsServer.EKLIPZ]: 'http://45.50.38.248:8080',
+  [GeneralsServer.LOCAL]: 'http://localhost:8080',
 };
 
 export const ADMINS = [
