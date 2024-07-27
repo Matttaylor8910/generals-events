7e9b77602beb15e04cb068a4629d813c729e7b2d
Matt Taylor <taylor.matt777@gmail.com>
2024-07-27 14:53:57 -0600

    Update test.js

--- functions/src/util/test.js
+++ functions/src/util/test.js
@@ -2,9 +2,10 @@ const simulator = require('./simulator');
 
 // Get the replayId from the command line arguments
 const replayId = process.argv[2];
+const server = process.argv[3] || 'na';
 
 if (replayId) {
-  simulator.getReplayStats(replayId).then(stats => {
+  simulator.getReplayStats(replayId, server).then(stats => {
     console.log(stats);
   });
 } else {
